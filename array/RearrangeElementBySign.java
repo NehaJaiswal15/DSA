f1bd376a080c426d252317a24589c5cf04a9a233
NehaJaiswal15 <nehajaiswal2850@gmail.com>
2026-01-29 20:36:40 +0530

    updated

--- array/RearrangeElementBySign.java
+++ array/RearrangeElementBySign.java
@@ -2,17 +2,15 @@ class Solution {
     public int[] rearrangeArray(int[] nums) {
         int n = nums.length;
         int[] ans = new int[n];
-        int pos = 0;
-        int neg = 1;
+        int pos = 0, neg = 1;
 
-        for(int i=0; i<n; i++){
-            if(nums[i] > 0){
-                ans[pos] = nums[i];
-                pos = pos+2;
-            }
-            else{
-                ans[neg] = nums[i];
-                neg = neg+2;
+        for (int num : nums) {
+            if (num > 0) {
+                ans[pos] = num;
+                pos += 2;
+            } else {
+                ans[neg] = num;
+                neg += 2;
             }
         }
         return ans;
