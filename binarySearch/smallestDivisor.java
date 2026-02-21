f027bf1c34c4b5bc44c90087695f89adec55410b
NehaJaiswal15 <nehajaiswal2850@gmail.com>
2026-02-21 20:39:54 +0530

    SmallestDivisor

---
+++ binarySearch/smallestDivisor.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int smallestDivisor(int[] nums, int threshold) {
+        int n = nums.length;
+        if(n>threshold) return -1;
+        int maxi = Integer.MIN_VALUE;
+        for(int i=0; i<n; i++){
+            maxi = Math.max(maxi, nums[i]);
+        }
+        int low = 1, high = maxi;
+        while(low<= high){
+            int mid = (low+high)/2;
+            if(sumOfDiv(nums, mid) <= threshold){
+                high = mid -1;
+            }else{
+                low = mid + 1;
+            }
+        }
+        return low;
+    }
+    int sumOfDiv(int[] nums, int mid){
+        int n = nums.length;
+        int sum = 0;
+        for(int i=0; i<n; i++){
+            sum += Math.ceil((double)nums[i] / mid);
+        }
+        return sum;
+    }
+}
