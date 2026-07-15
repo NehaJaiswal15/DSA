150278c7ffdd8edbb05f7d2593a3e428bc912bd5
NehaJaiswal15 <nehajaiswal2850@gmail.com>
2026-07-15 22:58:11 +0530

    fibonacci

---
+++ mathematical_problems/fibonacci.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class fibonacci{
+
+    public static int fibonacci(int n){
+        if(n == 0 || n == 1){
+            return n;
+        }
+
+        return fibonacci(n-1) + fibonacci(n-2);
+    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int num = sc.nextInt();
+
+        for(int i=0; i<num; i++){
+            System.out.print(fibonacci(i) + " ");
+        }
+    }
+}
